c4ea249cdf7aed6df69b3af5d9cedde1314013ea
Oleksandr <keysindie@gmail.com>
2020-11-05 21:04:39 +0200

    Add Message contructors

--- multi-paint/src/main/java/dhdhxji/connection_manager/Message.java
+++ multi-paint/src/main/java/dhdhxji/connection_manager/Message.java
@@ -2,4 +2,10 @@ package dhdhxji.connection_manager;
 
 public class Message {
     public String message;
+
+    public Message(String body) {
+        message = body;
+    }
+
+    public Message() {}
 }
