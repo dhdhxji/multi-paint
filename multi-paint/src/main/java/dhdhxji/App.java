c8a6c350eadab856a3d2802f32475c79539e6a3d
Oleksandr <keysindie@gmail.com>
2020-11-05 23:20:04 +0200

    Implement MVP

--- multi-paint/src/main/java/dhdhxji/App.java
+++ multi-paint/src/main/java/dhdhxji/App.java
@@ -1,13 +1,27 @@
 package dhdhxji;
 
-/**
- * Hello world!
- *
- */
+import dhdhxji.command.Resolver;
+import dhdhxji.connection_manager.ConnectionManager;
+import dhdhxji.pixmap.DrawInterface;
+import dhdhxji.pixmap.DrawPixmap;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Main draw interface
+        DrawInterface mainDrawer = new DrawPixmap(1920, 1080);
+        
+        //Command resolver: parse, deserialize and execute commands
+        Resolver mainResolver = new Resolver(mainDrawer);
+        
+        //  Connection manager. Manages User sessions, transfer
+        //data between clients and ProcessCommandListener interface
+        //(Resolver in this particular example)
+        ConnectionManager mainConnectionManager = 
+            new ConnectionManager(mainResolver, 3113);
+
+        //Run the server
+        mainConnectionManager.start();
     }
 }
