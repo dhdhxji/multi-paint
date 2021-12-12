5e106e0e23bb687f97af8f79ea663b50eb758691
Oleksandr <keysindie@gmail.com>
2021-12-13 01:18:20 +0200

    feat: Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,19 @@
 
 set -e
 
-terraform -chdir=tf apply -auto-approve
+az login &> /dev/null
 
+terraform -chdir=tf apply -auto-approve
 echo "$(terraform -chdir=tf output -raw kube_config)" > ./tf/azurek8s.yml
 
 export KUBECONFIG=./tf/azurek8s.yml
-kubectl get nodes
+
 kubectl apply -f kubernetes
-kubectl get svc
+
+external_ip=""
+while [ -z $external_ip ]; do
+    sleep 10
+    external_ip=$(kubectl get svc multi-paint --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+done
+
+echo "Multi-paint address: $external_ip"
